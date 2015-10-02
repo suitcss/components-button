9c67060ee39a860db5db8dd3fc0358b8a4427afb
Simon Smith <me@simonsmith.io>
2015-10-02 23:51:45 +0100

    Refactor build scripts to use PostCSS

---
+++ postcss-config.js
@@ -0,0 +1,18 @@
+var postcss = require('postcss');
+var bemLinter = require('postcss-bem-linter');
+var reporter = require('postcss-reporter');
+
+module.exports = {
+  use: [
+    "postcss-import",
+    "postcss-custom-properties",
+    "postcss-custom-media",
+    "postcss-calc",
+    "autoprefixer"
+  ],
+  "postcss-import": {
+    transform: function(css) {
+      return postcss([bemLinter, reporter]).process(css).css;
+    }
+  }
+};
